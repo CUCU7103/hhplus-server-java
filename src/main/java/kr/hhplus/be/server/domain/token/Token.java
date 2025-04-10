4d3f56ac36563e8bb8ecb1cf723118bdc3fb2f75
Joon Gyu <qkrwnsrb224@gmail.com>
2025-04-11 07:29:34 +0900

    [Feat] Token 도메인 클래스 추가
    Token 도메인 클래스 추가

--- src/main/java/kr/hhplus/be/server/domain/token/Token.java
+++ src/main/java/kr/hhplus/be/server/domain/token/Token.java
@@ -2,6 +2,8 @@ package kr.hhplus.be.server.domain.token;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,6 +16,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.global.error.CustomErrorCode;
+import kr.hhplus.be.server.global.error.CustomException;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,7 +31,7 @@ public class Token {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private long id;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = "status", nullable = false)
@@ -42,13 +46,17 @@ public class Token {
 	@Column(name = "modified_at", nullable = false)
 	private LocalDateTime modifiedAt;
 
+	@Column(name = "expiration_at")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime expirationAt;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@Builder
 	public Token(User user, LocalDateTime modifiedAt, LocalDateTime createdAt, String tokenValue, TokenStatus status,
-		Integer id) {
+		long id) {
 		this.user = user;
 		this.modifiedAt = modifiedAt;
 		this.createdAt = createdAt;
@@ -74,6 +82,28 @@ public class Token {
 	public Token updateStatus(TokenStatus status) {
 		this.status = status;
 		return this;
+	}
+
+	/**
+	 * 토큰이 WAITING 상태에서 외부에서 전달받은 waitingRank, activeTokenCount, maxActive 정보를 활용하여
+	 * 조건을 만족할 경우 ACTIVE 상태로 전환하는 메서드
+	 *
+	 * @param waitingRank      대기 순위
+	 * @param activeTokenCount 현재 ACTIVE 상태 토큰 수
+	 * @param maxActive        최대 허용 ACTIVE 토큰 수
+	 */
+
+	final long MAX_ACTIVE = 1000;
+
+	public void checkAndActivate(int waitingRank, long activeTokenCount) {
+		if (!TokenStatus.WAITING.equals(this.getStatus())) {
+			throw new CustomException(CustomErrorCode.INVALID_STATUS);
+		}
+		// 대기순위가 1이고, 현재 활성화 토큰 수가 maxActive 미만이면 ACTIVE 상태로 전환
+		if (waitingRank == 1 && activeTokenCount < MAX_ACTIVE) {
+			updateStatus(TokenStatus.ACTIVE);
+		}
+		// 그렇지 않으면 상태를 변경하지 않습니다.
 	}
 
 }
--- src/main/java/kr/hhplus/be/server/domain/token/TokenRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/token/TokenRepository.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.token;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +14,9 @@ public interface TokenRepository {
 	Optional<Token> findByUserId(long userId);
 
 	void save(Token token);
+
+	@Query("SELECT COUNT(t) FROM Token t WHERE t.status = 'WAITING' AND t.createdAt < (SELECT t2.createdAt FROM Token t2 WHERE t2.id = :tokenId)")
+	int getWaitingRank(long id);
+
+	long countByStatus(TokenStatus tokenStatus);
 }
