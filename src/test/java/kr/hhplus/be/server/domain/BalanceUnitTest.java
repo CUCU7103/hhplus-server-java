1e6a3390bb490e5fb094cbd4174e7959251f7e7a
Joon Gyu <qkrwnsrb224@gmail.com>
2025-04-11 02:30:55 +0900

    [Test] 결제 기능 테스트 코드 작성
    결제 기능 테스트 코드 작성

--- src/test/java/kr/hhplus/be/server/domain/BalanceUnitTest.java
+++ src/test/java/kr/hhplus/be/server/domain/BalanceUnitTest.java
@@ -11,7 +11,6 @@ import kr.hhplus.be.server.domain.balance.Balance;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.global.error.CustomErrorCode;
 import kr.hhplus.be.server.global.error.CustomException;
-import kr.hhplus.be.server.interfaces.balance.BalanceChargeRequest;
 
 class BalanceUnitTest {
 
@@ -19,15 +18,14 @@ class BalanceUnitTest {
 	void 최대포인트를_초과하면_포인트_충전에_실패한다() {
 		// arrange
 		long balanceId = 1L;
-		long chargePoint = 100000L;
+		BigDecimal chargePoint = BigDecimal.valueOf(100000);
 		Balance balance = Balance.builder()
 			.id(balanceId)
 			.point(BigDecimal.valueOf(1000))
 			.build();
-		BalanceChargeRequest request = new BalanceChargeRequest(balanceId, chargePoint);
 
 		// act & assert
-		assertThatThrownBy(() -> balance.chargePoint(request.toCommand().chargePoint())).isInstanceOf(
+		assertThatThrownBy(() -> balance.chargePoint(chargePoint)).isInstanceOf(
 				CustomException.class)
 			.hasMessageContaining(CustomErrorCode.OVER_CHARGED_POINT.getMessage());
 	}
@@ -36,14 +34,13 @@ class BalanceUnitTest {
 	void 최대포인트를_초과하지_않는다면_포인트_충전에_성공한다() {
 		// arrange
 		long balanceId = 1L;
-		long chargePoint = 9000L;
+		BigDecimal chargePoint = BigDecimal.valueOf(9000);
 		Balance balance = Balance.builder()
 			.id(balanceId)
 			.point(BigDecimal.valueOf(1000))
 			.build();
-		BalanceChargeRequest request = new BalanceChargeRequest(balanceId, chargePoint);
 		//act
-		Balance result = balance.chargePoint(request.toCommand().chargePoint());
+		Balance result = balance.chargePoint(chargePoint);
 
 		// assert
 		assertThat(result).isNotNull();
@@ -66,6 +63,41 @@ class BalanceUnitTest {
 		assertThat(user).isEqualTo(createBalance.getUser());
 		assertThat(BigDecimal.valueOf(1000)).isEqualTo(createBalance.getPoint());
 		assertThat(createBalance.getCreatedAt()).isNotNull();
+	}
+
+	@Test
+	void 보유한_포인트_이상_사용시_예외처리() {
+		// arrange
+		long balanceId = 1L;
+		BigDecimal usePoint = BigDecimal.valueOf(100000);
+
+		Balance balance = Balance.builder()
+			.id(balanceId)
+			.point(BigDecimal.valueOf(1000))
+			.build();
+
+		// assert
+		assertThatThrownBy(() -> balance.usePoint(usePoint)).isInstanceOf(
+				CustomException.class)
+			.hasMessageContaining(CustomErrorCode.OVER_USED_POINT.getMessage());
+	}
+
+	@Test
+	void 결제금액이_보유한_포인트_이하라면_사용가능() {
+		// arrange
+		long balanceId = 1L;
+		BigDecimal usePoint = BigDecimal.valueOf(5000);
+
+		Balance balance = Balance.builder()
+			.id(balanceId)
+			.point(BigDecimal.valueOf(20000))
+			.build();
+
+		Balance result = balance.usePoint(usePoint);
+		// assert
+		assertThat(result).isNotNull();
+		assertThat(result.getId()).isEqualTo(balanceId);
+		assertThat(result.getPoint()).isEqualTo(BigDecimal.valueOf(15000));
 	}
 
 }
