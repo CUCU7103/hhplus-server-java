18fbea1c230092829f78ebf60a2234dd96ca74ec
Joon Gyu <qkrwnsrb224@gmail.com>
2025-04-16 16:02:14 +0900

    [Feat] token 도메인 레포지토리 구현 1. TokenJpaRepository 생성 2. TokenRepositoryImpl 구현체 생성

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/token/TokenJpaRepository.java
@@ -0,0 +1,25 @@
+package kr.hhplus.be.server.infrastructure.token;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import kr.hhplus.be.server.domain.token.Token;
+import kr.hhplus.be.server.domain.token.TokenStatus;
+
+public interface TokenJpaRepository extends JpaRepository<Token, Long> {
+	Token getToken(long userId);
+
+	Optional<Token> findById(long tokenId);
+
+	Optional<Token> findByUserId(long userId);
+
+	@Query("SELECT COUNT(t) FROM Token t WHERE t.status = 'WAITING' AND t.createdAt < (SELECT t2.createdAt FROM Token t2 WHERE t2.id = :tokenId)")
+	int getWaitingRank(long id);
+
+	long countByStatus(TokenStatus tokenStatus);
+
+	List<Token> findAllByStatus(TokenStatus tokenStatus);
+}
