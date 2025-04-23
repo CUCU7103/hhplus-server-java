0c4f93fc5b3b01dd7224d9ae683f2b22f8999f5f
Joon Gyu <qkrwnsrb224@gmail.com>
2025-04-24 01:30:34 +0900

    [Feat] 포인트 충전 동시성 제어를 위한 비관적 락 적용 및 코드 수정

--- src/main/java/kr/hhplus/be/server/domain/balance/balance/BalanceRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/balance/BalanceRepository.java
@@ -2,11 +2,15 @@ package kr.hhplus.be.server.domain.balance.balance;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Lock;
+
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.balance.history.BalanceHistory;
 
 public interface BalanceRepository {
 	Optional<Balance> findById(long userId);
 
+	@Lock(LockModeType.OPTIMISTIC)
 	Optional<Balance> findByIdAndUserId(long balanceId, long userId);
 
 	BalanceHistory save(BalanceHistory history);
