2ae4f2b4cd96dfa4d778be581885730beed2518f
Joon Gyu <qkrwnsrb224@gmail.com>
2025-05-13 01:53:01 +0900

    [Fix] 전략 패턴에서 각 전략에 맞는 context를 생성하도록 수정

--- src/main/java/kr/hhplus/be/server/global/support/lock/model/LockAspect.java
+++ src/main/java/kr/hhplus/be/server/global/support/lock/model/LockAspect.java
@@ -6,12 +6,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.Ordered;
-import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.core.annotation.Order;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 import io.micrometer.observation.annotation.Observed;
@@ -29,38 +25,19 @@ import lombok.extern.slf4j.Slf4j;
 public class LockAspect {
 
 	private final LockFactory lockFactory; // 락 팩토리 주입
-	private final ExpressionParser parser = new SpelExpressionParser();
-	private final ParameterNameDiscoverer paramDiscoverer = new DefaultParameterNameDiscoverer();
 
 	@Around("@annotation(WithLock)")
 	@Observed(name = "withLock")
 	public Object around(ProceedingJoinPoint pjp) throws Throwable {
 		Method method = ((MethodSignature)pjp.getSignature()).getMethod();
 		WithLock withLock = method.getAnnotation(WithLock.class);
-
-		// String resolvedKey = resolveKey(withLock.key(), method, pjp.getArgs());
-		// log.error("KeyValue {} :", resolvedKey);
 		LockStrategy lockStrategy = lockFactory.getLock(withLock.type());
+
 		if (lockStrategy == null) {
 			throw new IllegalArgumentException("No LockStrategy found for type: " + withLock.type());
 		}
-
-		LockContext context = switch (withLock.type()) {
-			case REDIS_SIMPLE -> LockContext.createSimpleLockContext(withLock.key(), withLock.expireMillis());
-			case REDIS_SPIN -> LockContext.createSpinLockContext(
-				withLock.key(),
-				withLock.timeoutMillis(),
-				withLock.retryIntervalMillis(),
-				withLock.expireMillis()
-			);
-			case REDIS_PUBSUB -> LockContext.createPubSubLockContext(
-				withLock.key(),
-				withLock.timeoutMillis(),
-				withLock.expireMillis()
-			);
-			default -> throw new IllegalArgumentException("Unsupported lock type: " + withLock.type());
-		};
-
+		// 변경된 부분: LockContext 생성 책임을 LockStrategy에 위임
+		LockContext context = lockStrategy.createContext(withLock.key(), withLock);
 		boolean acquired = lockStrategy.lock(context);
 		if (!acquired) {
 			log.error("Key : {} 에서 락 획득에 실패하였습니다.", withLock.key());
@@ -75,16 +52,5 @@ public class LockAspect {
 			lockStrategy.unlock(context);
 		}
 	}
-
-	// private String resolveKey(String key, Method method, Object[] args) {
-	// 	EvaluationContext context = new StandardEvaluationContext();
-	// 	String[] paramNames = paramDiscoverer.getParameterNames(method);
-	// 	if (paramNames != null) {
-	// 		for (int i = 0; i < paramNames.length; i++) {
-	// 			context.setVariable(paramNames[i], args[i]);
-	// 		}
-	// 	}
-	// 	return parser.parseExpression(key).getValue(context, String.class);
-	// }
 
 }
