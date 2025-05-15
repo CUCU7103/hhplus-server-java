0ef3365e81076981ab592bcef0f15c669e54079e
Joon Gyu <qkrwnsrb224@gmail.com>
2025-05-15 17:21:52 +0900

    [Feat] 결제 로직에 이벤트 기능 추가

--- src/main/java/kr/hhplus/be/server/application/payment/PaymentService.java
+++ src/main/java/kr/hhplus/be/server/application/payment/PaymentService.java
@@ -1,10 +1,12 @@
 package kr.hhplus.be.server.application.payment;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.hhplus.be.server.domain.balance.balance.Balance;
 import kr.hhplus.be.server.domain.balance.balance.BalanceRepository;
+import kr.hhplus.be.server.domain.concert.ConcertRankRepository;
 import kr.hhplus.be.server.domain.payment.Payment;
 import kr.hhplus.be.server.domain.payment.PaymentRepository;
 import kr.hhplus.be.server.domain.reservation.Reservation;
@@ -13,6 +15,7 @@ import kr.hhplus.be.server.domain.token.Token;
 import kr.hhplus.be.server.domain.token.TokenRepository;
 import kr.hhplus.be.server.global.error.CustomErrorCode;
 import kr.hhplus.be.server.global.error.CustomException;
+import kr.hhplus.be.server.global.support.event.SearchRankEvent;
 import kr.hhplus.be.server.global.support.lock.model.LockType;
 import kr.hhplus.be.server.global.support.lock.model.WithLock;
 import lombok.RequiredArgsConstructor;
@@ -20,21 +23,22 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
-
+	private final ApplicationEventPublisher publisher;
 	private final BalanceRepository balanceRepository;
 	private final PaymentRepository paymentRepository;
 	private final ReservationRepository reservationRepository;
 	private final TokenRepository tokenRepository;
+	private final ConcertRankRepository rankRepository;
 
 	/**
-	 *  사용자의 잔여 포인트를 조회한다.
-	 *  잔여 포인트 조회 후 결제 여부를 판단한다.
-	 *  결제 금액이 부족한 경우 예외처리
-	 *  결제 금액이 충분한 경우
-	 *  포인트 차감 후 저장
-	 *  좌석 상태를 변경
-	 *  좌석의 결제 정보를 저장
-	 *  토큰의 상태를 만료 처리한다.
+	 * 사용자의 잔여 포인트를 조회한다.
+	 * 잔여 포인트 조회 후 결제 여부를 판단한다.
+	 * 결제 금액이 부족한 경우 예외처리
+	 * 결제 금액이 충분한 경우
+	 * 포인트 차감 후 저장
+	 * 좌석 상태를 변경
+	 * 좌석의 결제 정보를 저장
+	 * 토큰의 상태를 만료 처리한다.
 	 */
 	@WithLock(
 		key = "'payment:' + #userId + ':' + #reservationId",
@@ -58,6 +62,8 @@ public class PaymentService {
 		// 예약 도메인에서 좌석의 상태를 변경하는 책임을 가지고 있기에 예약 확정!
 		Payment payment = paymentRepository.saveAndFlush(Payment
 			.createPayment(reservation, command.amount(), balance));
+		// 이벤트 발행
+		publisher.publishEvent(new SearchRankEvent(this, reservation.getConcertSchedule().getId()));
 		return PaymentInfo.from(payment);
 	}
 
